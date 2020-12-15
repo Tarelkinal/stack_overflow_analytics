cee96c123fd2746f2baae1e2dfb3ccee46f042a4
Tarelkinal <alekandr.tarelkin@gmail.com>
2020-12-15 16:28:20 +0300

    first commit

---
+++ task_Tarelkin_Aleksandr_stackoverflow_analytics.py
@@ -0,0 +1,170 @@
+#!C:\Users\tarel\Anaconda3\envs\python_advanced\python.exe
+"""Module for calculation words popularity score over Stackoverflow posts"""
+import csv
+import json
+import logging
+import logging.config
+import re
+import sys
+import yaml
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from collections import defaultdict
+from lxml import etree
+
+
+STOP_WORDS_FPATH = 'stop_words_in_koi8r.txt'
+LOGGING_CONFIG_YAML = 'logging.conf.yml'
+APPLICATION_NAME = 'task_Tarelkin_Aleksandr_stackoverflow_analytics'
+
+
+logger = logging.getLogger(APPLICATION_NAME)
+
+
+class WordPopularityIndex:
+    """
+    Provide functionality to processing queries to stackoverflow word popularity index
+
+    use WordPopularityIndex.query to find top words based on their popularity during period provided
+
+    Attributes
+    ----------
+    word_popularity_index: keep word popularity index by years
+    """
+    def __init__(self, word_popularity_index: dict):
+        """construct object of class with word popularity index built by years"""
+        self.word_popularity_index = word_popularity_index
+
+    def query(self, start_year: int, end_year: int, top_len: int):
+        """
+        Function to query word popularity index
+
+        :param start_year: start year for considering period
+        :param end_year: last year for considering period (included)
+        :param top_len: number of top words based on score popularity
+        :return: JSON of the form {
+            "start": start_year,
+            "end": end_year,
+            "top": [["top_1_word": top_1_word_score], ..., ["top_n_word": top_n_word_score]]
+        }
+        """
+        logger.debug('got query "%s,%s,%s"', repr(start_year), repr(end_year), repr(top_len))
+
+        answer_dict = defaultdict(int)
+        for year in range(start_year, end_year + 1):
+            if year in self.word_popularity_index.keys():
+                for word, score in self.word_popularity_index[year].items():
+                    answer_dict[word] += score
+        if len(answer_dict) < top_len:
+            logger.warning(
+                'not enough data to answer, found '
+                'top_%s words out of top_%s for period "%s,%s"',
+                len(answer_dict),
+                top_len,
+                start_year,
+                end_year
+            )
+            top_len = len(answer_dict)
+        answer_list = [[w, s] for w, s in answer_dict.items()]
+        pre_sort = sorted(answer_list, key=lambda x: x[0])
+        answer_list = sorted(pre_sort, key=lambda x: x[1], reverse=True)[:top_len]
+        answer = json.dumps({"start": start_year, "end": end_year, "top": answer_list})
+
+        return answer
+
+
+def load_xml_documents(fpath: str) -> list:
+    """Return list of etree objects"""
+    with open(fpath, 'rb') as f_in:
+        documents = [etree.fromstring(x.strip().decode(), etree.XMLParser()) for x in f_in.readlines()]
+
+    return documents
+
+
+def build_word_popularity_index(documents: list, stop_words_path=STOP_WORDS_FPATH) -> WordPopularityIndex:
+    """
+    Return WordPopularityIndex class object initialized with
+    word popularity index built by years
+    """
+    word_popularity_index = defaultdict(lambda: defaultdict(int))
+
+    with open(stop_words_path, 'r', encoding='koi8-r') as f_sw_in:
+        stop_words_list = [x.strip() for x in f_sw_in.readlines()]
+
+    for elem in documents:
+        if elem.xpath('@PostTypeId')[0] == '2':
+            continue
+        score = int(elem.xpath('@Score')[0])
+        year = int(elem.xpath('@CreationDate')[0][:4])
+        words = re.findall(r'\w+', elem.xpath('@Title')[0].lower())
+        for word in set(words):
+            if word in stop_words_list:
+                continue
+            word_popularity_index[year][word] += score
+
+    word_popularity_index_built = WordPopularityIndex(word_popularity_index)
+
+    return word_popularity_index_built
+
+
+def callback_query(dataset_fpath: str, stop_words_fpath: str, queries_fpath: str):
+    """
+    Process commands received from CLI
+    """
+    documents = load_xml_documents(dataset_fpath)
+    word_popularity_index = build_word_popularity_index(documents, stop_words_fpath)
+
+    logger.info('process XML dataset, ready to serve queries')
+
+    queries_list = []
+    with open(queries_fpath, 'r') as f_in:
+        reader = csv.reader(f_in, delimiter=',')
+        for row in reader:
+            queries_list.append(list(map(int, row)))
+
+    for query in queries_list:
+        query_result = word_popularity_index.query(*query)
+        print(f'{query_result}', file=sys.stdout)
+
+    logger.info('finish processing queries')
+
+
+def setup_parser(parser):
+    parser.add_argument(
+        "--questions", required=True, dest="dataset_fpath",
+        help="path to stackoverflow dataset to load"
+    )
+    parser.add_argument(
+        "--stop-words", required=True, dest="stop_words_fpath",
+        help="path to stop words document in koi8-r encoding to exclude them from popularity index"
+    )
+    parser.add_argument(
+        "--queries", required=True, dest="queries_fpath",
+        help="path to queries to query them against popularity index"
+    )
+    parser.set_defaults(callback=callback_query)
+
+
+def setup_logging():
+    with open(LOGGING_CONFIG_YAML) as config_fin:
+        logging.config.dictConfig(yaml.safe_load(config_fin))
+
+
+def main():
+    setup_logging()
+    parser = ArgumentParser(
+        prog="word-popularity-index",
+        description="tool to build and query word-popularity index for stackoverflow dataset",
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+    setup_parser(parser)
+    arguments = parser.parse_args()
+
+    arguments.callback(
+        arguments.dataset_fpath,
+        arguments.stop_words_fpath,
+        arguments.queries_fpath,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_Tarelkin_Aleksandr_stackoverflow_analytics.py
@@ -0,0 +1,165 @@
+from textwrap import dedent
+from lxml import etree
+import json
+from argparse import Namespace
+from unittest.mock import patch
+import csv
+
+import pytest
+
+from task_Tarelkin_Aleksandr_stackoverflow_analytics import (
+    load_xml_documents,
+    build_word_popularity_index,
+    ArgumentParser,
+    callback_query,
+    main,
+    STOP_WORDS_FPATH,
+)
+
+QUERIES_FPATH = 'queries_sample.csv'
+STACKOVERFLOW_POSTS_FPATH = 'stackoverflow_posts_sample.xml'
+DATASET_TINY_XML = dedent("""\
+    <row PostTypeId="1" CreationDate="2008-10-15" Score="1" Title="how linux linux?" />	
+    <row PostTypeId="1" CreationDate="2008-10-15" Score="12" Title="How command work in Linux?" />
+    <row PostTypeId="1" CreationDate="2009-10-15" Score="5" Title="How command work in Unix?" />	
+    <row PostTypeId="1" CreationDate="2009-10-15" Score="-4" Title="How command work in Linux?" />	
+    <row PostTypeId="2" CreationDate="2008-10-15" Score="12"/>\
+""")
+QUERIES_SAMPLE = [
+    [2008, 2008, 2],
+    [2008, 2008, 4],
+    [2008, 2009, 3],
+    [2010, 2012, 1]
+]
+QUERIES_SAMPLE_2 = [
+    [2008, 2008, 20],
+    [2008, 2009, 4],
+    [2008, 2010, 10],
+    [2009, 2009, 30],
+    [2008, 2020, 10]
+]
+ANSWER_FOR_QUERIES_SAMPLE_2 = """\
+{"start": 2008, "end": 2008, "top": [["using", 257], ["concatenate", 243], ["linq", 243], ["strings", 243], ["file", 178], ["create", 171], ["text", 167], ["batch", 164], ["database", 91], ["c", 85], ["enums", 85], ["save", 85], ["ways", 85], ["write", 79], ["literal", 74], ["short", 74], ["80", 62], ["characters", 62], ["lines", 62], ["make", 62]]}
+{"start": 2008, "end": 2009, "top": [["using", 257], ["concatenate", 243], ["linq", 243], ["strings", 243]]}
+{"start": 2008, "end": 2010, "top": [["using", 321], ["ways", 293], ["text", 261], ["concatenate", 243], ["linq", 243], ["strings", 243], ["implement", 218], ["data", 214], ["mongodb", 208], ["versioning", 208]]}
+{"start": 2009, "end": 2009, "top": []}
+{"start": 2008, "end": 2020, "top": [["using", 321], ["ways", 293], ["text", 261], ["concatenate", 243], ["linq", 243], ["strings", 243], ["implement", 218], ["data", 214], ["mongodb", 208], ["versioning", 208]]}"""
+
+
+@pytest.fixture()
+def tiny_dataset_fio(tmpdir):
+    dataset_fio = tmpdir.join("dataset.xml")
+    dataset_fio.write(DATASET_TINY_XML)
+    return dataset_fio
+
+
+@pytest.fixture()
+def queries_sample_fio(tmpdir):
+    queries_fio = tmpdir.join("queries_sample.csv")
+    with open(queries_fio, 'w', newline='') as f_in:
+        writer = csv.writer(f_in, delimiter=',')
+        for query in QUERIES_SAMPLE:
+            writer.writerow(query)
+    return queries_fio
+
+
+def test_can_load_xml_documents(tiny_dataset_fio):
+    documents = load_xml_documents(tiny_dataset_fio)
+    etalon_documents = [
+        etree.fromstring(
+            '<row PostTypeId="1" CreationDate="2008-10-15" Score="1" Title="how linux linux?" />',
+            etree.XMLParser()),
+        etree.fromstring(
+            '<row PostTypeId="1" CreationDate="2008-10-15" Score="12" Title="How command work in Linux?" />',
+            etree.XMLParser()),
+        etree.fromstring(
+            '<row PostTypeId="1" CreationDate="2009-10-15" Score="5" Title="How command work in Unix?" />',
+            etree.XMLParser()),
+        etree.fromstring(
+            '<row PostTypeId="1" CreationDate="2009-10-15" Score="-4" Title="How command work in Linux?" />',
+            etree.XMLParser()),
+        etree.fromstring(
+            '<row PostTypeId="2" CreationDate="2008-10-15" Score="12"/>',
+            etree.XMLParser()),
+    ]
+    assert len(etalon_documents) == len(documents), "load_xml_documents incorrectly loaded dataset"
+    assert sum([0 if x.values() == y.values() else 1 for x, y in zip(etalon_documents, documents)]) == 0, (
+        "load_xml_documents incorrectly loaded dataset"
+    )
+
+
+@pytest.fixture()
+def stackoverflow_posts_sample_documents():
+    documents = load_xml_documents(STACKOVERFLOW_POSTS_FPATH)
+    return documents
+
+
+def test_can_load_stackoverflow_posts_sample(stackoverflow_posts_sample_documents):
+    documents = stackoverflow_posts_sample_documents
+    assert len(documents) == 1120, "stackoverflow_posts_sample was loaded incorrectly"
+
+
+def test_world_popularity_index_can_return_right_answer(stackoverflow_posts_sample_documents):
+    documents = stackoverflow_posts_sample_documents
+    word_popularity_index = build_word_popularity_index(documents)
+    answer = []
+    for query in QUERIES_SAMPLE_2:
+        answer.append(word_popularity_index.query(*query))
+    res_answer = '\n'.join(answer)
+
+    assert res_answer == ANSWER_FOR_QUERIES_SAMPLE_2, (
+        f"True answer if {ANSWER_FOR_QUERIES_SAMPLE_2}, but got \n{res_answer}"
+    )
+
+
+@pytest.mark.parametrize(
+    "query, etalon_answer",
+    [
+        pytest.param(
+            [2008, 2008, 2],
+            json.dumps({"start": 2008, "end": 2008, "top": [["linux", 13], ["command", 12]]}),
+            id="2008 top 2"
+        ),
+        pytest.param(
+            [2008, 2008, 4],
+            json.dumps({"start": 2008, "end": 2008, "top": [["linux", 13], ["command", 12], ["work", 12]]}),
+            id="2008 top 4 - not enough data to answer"
+        ),
+        pytest.param(
+            [2008, 2009, 3],
+            json.dumps({"start": 2008, "end": 2009, "top": [["command", 13], ["work", 13], ["linux", 9]]}),
+            id="2008/2009 top 3"
+        ),
+        pytest.param(
+            [2010, 2012, 1],
+            json.dumps({"start": 2010, "end": 2012, "top": []}),
+            id="2010/2012 top 1 - not enough data to answer"
+        ),
+
+    ]
+)
+def test_query_word_popularity_index(tiny_dataset_fio, query, etalon_answer):
+    documents = load_xml_documents(tiny_dataset_fio)
+    tiny_word_popularity_index = build_word_popularity_index(documents)
+    answer = tiny_word_popularity_index.query(*query)
+    assert answer == etalon_answer, (
+        f"Expected answer is {etalon_answer}, but got {answer}"
+    )
+
+
+def test_main_can_properly_ran_all_process(tiny_dataset_fio, queries_sample_fio, capsys, caplog):
+    with patch.object(
+            ArgumentParser,
+            "parse_args",
+            return_value=Namespace(
+                dataset_fpath=tiny_dataset_fio,
+                stop_words_fpath=STOP_WORDS_FPATH,
+                queries_fpath=queries_sample_fio,
+                callback=callback_query,
+            )
+    ):
+        main()
+    captured = capsys.readouterr()
+    from pdb import set_trace; set_trace()
+    assert '["command", 13], ["work", 13], ["linux", 9]' in captured.out
+    assert '' == captured.err, "service messages must no be in stderr"
